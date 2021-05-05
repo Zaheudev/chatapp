764e2c221417228ab515b5ef7504177b2db69dbc
Zaheudev <68231115+Zaheudev@users.noreply.github.com>
2021-05-05 20:32:22 +0300

    Solved the problem when the data is not updated after joined or creating a room

--- chat-app/public/javascripts/roomPage.js
+++ chat-app/public/javascripts/roomPage.js
@@ -4,10 +4,16 @@ const sendButton = document.querySelector("#sendButton");
 
 client.onopen = function(event) {
     client.send(JSON.stringify(new Message("joined")));
+
 }
 
-sendButton.addEventListener('click', (e) => {
+setTimeout(()=>{
+    if(document.querySelector("#list").childElementCount == 0){
+        location.reload();
+    }
+}, 1000);
 
+sendButton.addEventListener('click', (e) => {
     if(textField.value.length != 0){
         client.send(JSON.stringify(new Message("MESSAGE", {text: textField.value, from: realusr, code: roomCode})));
         textField.value = null;
